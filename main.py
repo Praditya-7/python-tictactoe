7054171af868e36392a4954d4d04ad811bc83295
Praditya-7 <mananadhar.praditya7@gmail.com>
2026-02-24 23:00:58 +0545

    created board

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print('This is tictac toe')
+
+class Board:
+    def __init__(self):
+        self.board = ['']*9
+
+    def display(self):
+        print("   |   |   ")
+        print("---+---+---")
+        print("   |   |   ")
+        print("---+---+---")
+        print("   |   |   ")
+
+b1 =Board()
+b1.display()
