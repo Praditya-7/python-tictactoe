3a0986015e7e4cbf51d7cec4ef87bec868039642
Praditya-7 <mananadhar.praditya7@gmail.com>
2026-02-25 23:07:16 +0545

    added guide postions and board with X and O

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ class Board:
         self.board = ['']*9
 
     def display(self):
-        print("   |   |   ")
+        b=self.cells
+        print()
+        print(f" {b[0]} | {b[1]} | {b[2]}  ")
         print("---+---+---")
-        print("   |   |   ")
+        print(f" {b[3]} | {b[4]} | {b[5]}  ")
         print("---+---+---")
-        print("   |   |   ")
+        print(f" {b[6]} | {b[7]} | {b[8]} ")
 
 b1 =Board()
+b1.cells = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
 b1.display()
+
+guide = Board()
+guide.cells = [str(i) for i in range(1,10)]
+guide.display()
